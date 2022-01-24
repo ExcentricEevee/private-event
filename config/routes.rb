18cd79d93eba8823f0ce4013c78ae217736d1114
Curtis <excentriceevee@hotmail.com>
2022-01-24 09:50:46 -0500

    Added creator_id to Event table, fixing the association issue with User. New Event form now correctly does its job and fills information appropriately! TODO: add validations to prevent things such as nil fields. Begin constructing Event Attendance.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "events#index"
-  resources :events, only: [:index, :new, :create]
+  resources :events, only: [:index, :show, :new, :create]
   resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_052700) do
+ActiveRecord::Schema.define(version: 2022_01_22_144009) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_01_22_052700) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
+    t.bigint "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
