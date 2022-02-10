993e4468c90a5c96cfff3e84621d23ace2004c8e
Curtis <excentriceevee@hotmail.com>
2022-02-10 16:25:23 -0500

    Implement showing list of attended events on User pages, and list of Attendees on event pages. Still working on the Attend button interaction, but confirmed through rails console that EventAttendee model works as intended.

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -4,6 +4,8 @@ class EventAttendeesController < ApplicationController
     end
 
     def create
+        #@event = Event.find(params[:event_id])
         @event_attendee = EventAttendee.new(event_attendee_id: current_user.id, attended_event_id: @event.id)
+
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
-        @events = @user.events
+        @events_attended = EventAttendee.where(attended_event_id: @user.id)
     end
 end
