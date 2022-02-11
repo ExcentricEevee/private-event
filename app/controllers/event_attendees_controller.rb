92038ecde6b691519747a7ac5c01770488f6b9da
Curtis <excentriceevee@hotmail.com>
2022-02-11 02:15:25 -0500

    Remove redundent code. Paving the way for validations and logic checking, such as not allowing duplicate attendee:event pairs.

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -1,12 +1,10 @@
 class EventAttendeesController < ApplicationController
-    def new
-
-    end
-
     def create
+        #:event_id is passed from the link_to inside views/events/show.html.erb
         @event = Event.find(params[:event_id])
-        @event_attendee = EventAttendee.new(event_attendee_id: current_user.id, attended_event_id: @event.id)
-        @event_attendee.save
+        #use #create instead of #new and #save since we're not relying on user input, and a user MUST be signed in
+        #TODO: check to see if the user:event pair already exists in database; if so, do not create another one.
+        @event_attendee = EventAttendee.create(event_attendee_id: current_user.id, attended_event_id: @event.id)
         redirect_to @event
     end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,6 @@
 class EventsController < ApplicationController
 
     def index
-        @user = current_user
         @events = Event.all
     end
 
